a1b1b9da57529a0edab1a5185a0a077e38701457
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-03-12 23:05:29 +0900

    Initial commit

---
+++ p1/2/sketch.js
@@ -0,0 +1,20 @@
+var stepX;
+var stepY;
+
+function setup() {
+  createCanvas(800, 400);
+  noStroke();
+  colorMode(HSB, width, height, 100);
+}
+
+function draw() {
+  stepX = mouseX + 2;
+  stepY = mouseY + 2;
+
+  for (var gridY = 0; gridY < height; gridY += stepY) {
+    for (var gridX = 0; gridX < width; gridX += stepX) {
+      fill(gridX, height - gridY, 100);
+      rect(gridX, gridY, stepX, stepY);
+    }
+  }
+}
